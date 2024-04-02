cf66078d94efd1d6cc748803b2d810fe21e10541
elder-plinius <plinius8@proton.me>
2024-04-02 15:13:17 -0700

    example fix

--- example.py
+++ example.py
@@ -1,1 +1,22 @@
-____________________
+from swarms import Agent, Anthropic
+
+
+## Initialize the workflow
+agent = Agent(
+    agent_name="Transcript Generator",
+    agent_description=(
+        "Generate a transcript for a youtube video on what swarms"
+        " are!"
+    ),
+    llm=Anthropic(),
+    max_loops=3,
+    autosave=True,
+    dashboard=False,
+    streaming_on=True,
+    verbose=True,
+    stopping_token="<DONE>",
+    interactive=True,
+)
+
+# Run the workflow on a task
+agent("Generate a transcript for a youtube video on what swarms are!")
